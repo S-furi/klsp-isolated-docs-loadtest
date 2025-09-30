f4b25d8d733c94cad0dd261baa6db0739c403303
Stefano Furi <stefano.furi@jetbrains.com>
2025-09-30 08:57:07 +0200

    chore: run WS comparative analysis upon new test scenario

--- src/main/kotlin/TestRunner.kt
+++ src/main/kotlin/TestRunner.kt
@@ -8,31 +8,23 @@ import kotlin.time.measureTime
 import MemoryMetricsCollector.TestType
 
 object TestRunner {
-    private val scope = CoroutineScope(context = SupervisorJob() + Dispatchers.IO)
-
     suspend fun simpleJvmTest() {
-        scope.launch {
-            MemoryMetricsCollector.runCollectingJvmMetricsStdout {
-                val time = measureTime {
-                    LoadTest.performNParallelCompletionsChunked(10_000, chunkSize = 1_000)
-                }
-                println("Total time: $time")
+        MemoryMetricsCollector.runCollectingJvmMetricsStdout {
+            val time = measureTime {
+                LoadTest.performNParallelCompletionsChunked(10_000, chunkSize = 1_000)
             }
-        }.join()
+            println("Total time: $time")
+        }
     }
 
     suspend fun k6MassiveTestToJsonOutput(testType: TestType = TestType.REST_BASIC) {
         val outFile = "results-jvm-rest.json"
-        scope.launch {
-            MemoryMetricsCollector.runK6CollectingJvmMetricsJson(testType, outputJson = outFile)
-        }.join()
+        MemoryMetricsCollector.runK6CollectingJvmMetricsJson(testType, outputJson = outFile)
         DataHandler.plotMemoryMetrics(outFile)
     }
 
     suspend fun k6MassiveTestForGrafanaVisualisation(testType: TestType = TestType.REST_BASIC) {
-        scope.launch {
-            MemoryMetricsCollector.runK6CollectingJvmMetricsJson(testType, withInfluxDb = true)
-        }.join()
+        MemoryMetricsCollector.runK6CollectingJvmMetricsJson(testType, withInfluxDb = true)
     }
 }
 
